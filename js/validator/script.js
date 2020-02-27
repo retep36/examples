b1488e0396a931e16187f1d25d022a448683d824
Peter Varchola <peter.varchola@gmail.com>
2020-02-27 12:32:47 +0100

    added CSS and Bootstrap

--- js/validator/script.js
+++ js/validator/script.js
@@ -7,11 +7,13 @@ function validateForm(){
 
 	if(tmp){
 		document.getElementById('email2').disabled = false;
-		emailValid.innerHTML = "YES";
+		emailValid.innerHTML = "VALID EMAIL";
+		emailValid.style.color = '#33cc33';
 		status1 = true;
 	}
 	else {
-		emailValid.innerHTML = "NO";
+		emailValid.innerHTML = "NOT VALID EMAIL";
+		emailValid.style.color = 'tomato';
 		status1 = false;
 	}
 
@@ -20,11 +22,13 @@ function validateForm(){
 	var emailValid2 = document.getElementById('emailValid2');
 	var tmp = email1.localeCompare(email2);
 	if(email1 == email2){
-		emailValid2.innerHTML = "EQUAL";
+		emailValid2.innerHTML = "EQUAL EMAILS";
+		emailValid2.style.color = '#33cc33';
 		status2 = true;
 	}
 	else if(status1 == true){
-		emailValid2.innerHTML = "NOT EQUAL";
+		emailValid2.innerHTML = "NOT EQUAL EMAILS";
+		emailValid.style.color = 'tomato';
 		status2 = false;
 	}
 
@@ -34,10 +38,12 @@ function validateForm(){
 
 	if(regExp){
 		strength.innerHTML = "Strong and good password!";
+		strength.style.color = '#33cc33';
 		status3 = true;
 	}
 	else if(heslo.length > 0) {
-		strength.innerHTML = "Bad password! Pass must contain at least one big letter, one small letter and one number";
+		strength.innerHTML = "Bad password! Pass must contain big letter, small letter and number";
+		strength.style.color = 'tomato';
 		status3 = false;
 	}
 	else if(heslo.length == 0){

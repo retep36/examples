43073eae02ba4338faa8fd5bed1787daabab44c5
Peter Varchola <peter.varchola@gmail.com>
2020-08-01 21:25:09 +0200

    uprava js na es6, css upravene

--- js/slider/script.js
+++ js/slider/script.js
@@ -1,33 +1,34 @@
-var images = [
-	'<img src="img1.jpg">', //0
-	'<img src="img2.jpg">', //1
-	'<img src="img3.jpg">', //2
-	'<img src="img4.jpg">'  //3
+const images = [
+  '<img class="image-slider cropp" src="assets/img1.jpg">', //0
+  '<img class="image-slider cropp" src="assets/img2.jpg">', //1
+  '<img class="image-slider cropp" src="assets/img3.jpg">', //2
+  '<img class="image-slider cropp" src="assets/img4.jpg">', //3
 ];
 
-var time, index = 0;
+let numberOfTimer,
+  index = 0;
 
 slideShow();
 
 function slideShow() {
-	document.getElementById('slider').innerHTML = images[index];
-	index = index + 1;
-	index == images.length ? index = 0 : null;
-	time = setTimeout(function() { slideShow() }, 3000);
+  document.getElementById("slider").innerHTML = images[index];
+  index = ++index;
+  index == images.length ? (index = 0) : null;
+  numberOfTimer = setTimeout(() => slideShow(), 3000);
 }
 
 function prev() {
-	clearTimeout(time);
-	var tmp = index - 2;
-	tmp == -2 ? tmp = images.length - 2 : false;
-	tmp == -1 ? tmp = images.length - 1 : false;
-	index = tmp;
-	document.getElementById('slider').innerHTML = images[index];
-	slideShow();	
+  clearTimeout(numberOfTimer);
+  let tmp = index - 2;
+  tmp == -2 ? (tmp = images.length - 2) : false;
+  tmp == -1 ? (tmp = images.length - 1) : false;
+  index = tmp;
+  document.getElementById("slider").innerHTML = images[index];
+  slideShow();
 }
 
 function next() {
-	clearTimeout(time);
-	document.getElementById('slider').innerHTML = images[index];
-	slideShow();
+  clearTimeout(numberOfTimer);
+  document.getElementById("slider").innerHTML = images[index];
+  slideShow();
 }

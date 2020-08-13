ea28ced48fd59118b87e773c30284cb84c570e3e
Peter Varchola <peter.varchola@gmail.com>
2020-08-13 13:38:08 +0200

    zadanie - produkt

--- js/validator/script.js
+++ js/validator/script.js
@@ -1,61 +1,66 @@
-function validateForm(){
-	var status1, status2, status3;
+function validateForm() {
+  let status1, status2, status3;
+  let email1 = document.getElementById("email1").value;
+  let email2 = document.getElementById("email2").value;
 
-	var email1 = document.getElementById('email1').value;
-	var tmp = email1.match(/.+@([a-z])+\.[a-z]{2,3}/g);
-	var emailValid = document.getElementById('emailValid');
+  // functions to valid and check
+  (email1) => {
+    let goodEmailAdress = email1.match(/.+@([a-z])+\.[a-z]{2,3}/g);
+    let emailValid = document.getElementById("emailValid");
 
-	if(tmp){
-		document.getElementById('email2').disabled = false;
-		emailValid.innerHTML = "VALID EMAIL";
-		emailValid.style.color = '#33cc33';
-		status1 = true;
-	}
-	else {
-		emailValid.innerHTML = "NOT VALID EMAIL";
-		emailValid.style.color = 'tomato';
-		status1 = false;
-	}
+    if (goodEmailAdress) {
+      document.getElementById("email2").disabled = false;
+      emailValid.innerHTML = "VALID EMAIL";
+      emailValid.style.color = "#33cc33";
+      status1 = true;
+    } else {
+      document.getElementById("email2").disabled = true;
+      emailValid.innerHTML = "NOT VALID EMAIL";
+      emailValid.style.color = "#ff6347";
+      status1 = false;
+    }
+  };
 
-	var email1 = document.getElementById('email1').value;
-	var email2 = document.getElementById('email2').value;
-	var emailValid2 = document.getElementById('emailValid2');
-	var tmp = email1.localeCompare(email2);
-	if(email1 == email2){
-		emailValid2.innerHTML = "EQUAL EMAILS";
-		emailValid2.style.color = '#33cc33';
-		status2 = true;
-	}
-	else if(status1 == true){
-		emailValid2.innerHTML = "NOT EQUAL EMAILS";
-		emailValid.style.color = 'tomato';
-		status2 = false;
-	}
+  checkEmail(email1, email2);
+  validPass();
 
-	var heslo = document.getElementById('pass').value;
-	var regExp = heslo.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g);
-	var strength = document.getElementById('strength');
+  if (status1 && status2 && status3) {
+    document.getElementById("odosli").disabled = false;
+  } else {
+    document.getElementById("odosli").disabled = true;
+  }
 
-	if(regExp){
-		strength.innerHTML = "Strong and good password!";
-		strength.style.color = '#33cc33';
-		status3 = true;
-	}
-	else if(heslo.length > 0) {
-		strength.innerHTML = "Bad password! Pass must contain big letter, small letter and number";
-		strength.style.color = 'tomato';
-		status3 = false;
-	}
-	else if(heslo.length == 0){
-		strength.innerHTML = "";
-		status3 = false;
-	}
+  function checkEmail(email1, email2) {
+    let emailValid2 = document.getElementById("emailValid2");
+    console.log(status1);
+    if (email1 === email2 && email1.length > 3) {
+      emailValid2.innerHTML = "EQUAL EMAILS";
+      emailValid2.style.color = "#33cc33";
+      status2 = true;
+    } else if (status1) {
+      emailValid2.innerHTML = "NOT EQUAL EMAILS";
+      emailValid.style.color = "#ff6347";
+      status2 = false;
+    }
+  }
 
+  function validPass() {
+    let heslo = document.getElementById("pass").value;
+    let regExp = heslo.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g);
+    let strength = document.getElementById("strength");
 
-	if(status1 && status2 && status3){
-		document.getElementById('odosli').disabled = false;
-	}
-	else {
-		document.getElementById('odosli').disabled = true;
-	}
+    if (regExp) {
+      strength.innerHTML = "Strong and good password!";
+      strength.style.color = "#33cc33";
+      status3 = true;
+    } else if (heslo.length > 0) {
+      strength.innerHTML =
+        "Bad password! Pass must contain big letter, small letter and number";
+      strength.style.color = "tomato";
+      status3 = false;
+    } else if (heslo.length == 0) {
+      strength.innerHTML = "";
+      status3 = false;
+    }
+  }
 }
---
+++ vue/zadanie/precache-manifest.82d22866b47f97832e702c8963e2ae33.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65e051ac9b46c576c40",
+    "url": "/css/app.c1127d12.css"
+  },
+  {
+    "revision": "0eb8ca985cbfe3fe5143",
+    "url": "/css/chunk-vendors.5ff60f72.css"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/fonts/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/fonts/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/fonts/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/fonts/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/fonts/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/fonts/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/fonts/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/fonts/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/fonts/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/fonts/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/fonts/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/fonts/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/img/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/img/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/img/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "ba7939245ab60e21178fce7ad181dfdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65e051ac9b46c576c40",
+    "url": "/js/app.13a5811b.js"
+  },
+  {
+    "revision": "0eb8ca985cbfe3fe5143",
+    "url": "/js/chunk-vendors.36fe8d8a.js"
+  },
+  {
+    "revision": "7208a8c4f155617cb3ae56c34278ee99",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
